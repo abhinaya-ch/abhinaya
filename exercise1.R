14c2e54010c17aad02bb42c9a7bfc8892facea08
abhinaya-ch <228w1a1275@vrsec.ac.in>
2024-02-19 03:07:29 -0800

    commit

---
+++ exercise1.R
@@ -0,0 +1,7 @@
+Student_Name<-c("Abhi","Ravi","Pooji","Ram")
+Age<-c(19,18,17,20)
+Weight<-c(45,50,52,54)
+Height<-c(142,150,160,145)
+Gender<-c("Male","Female","Male","Female")
+df<-data.frame(Student_Name,Age,Weight,Height,Gender)
+df
